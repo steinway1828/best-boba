98e7770d4179c024a5f627c7447be5c5571e45dd
steinway1828 <steinway1828@gmail.com>
2019-07-06 14:55:45 -0700

    appv4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { Page2Component } from './page2/page2.component';
 import { Page1Component } from './page1/page1.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
 ];
