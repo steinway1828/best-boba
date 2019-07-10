cc322e89293e67e3814c343322f24c66cfb20626
steinway1828 <steinway1828@gmail.com>
2019-07-10 12:17:07 -0700

    appv7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { Page2Component } from './page2/page2.component';
-import { Page1Component } from './page1/page1.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
+  { path: '', pathMatch:'full', redirectTo: 'dashboard' },
+  { path: '**', redirectTo: 'dashboard'}
 ];
 
 @NgModule({
